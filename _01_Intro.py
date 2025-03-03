c7be2993c0a78b5a124c23f978d7df263e232051
UtkarshSinghShorthillsAI <utkarsh.singh@shorthills.ai>
2025-03-03 11:30:23 +0530

    setup python

---
+++ _01_Intro.py
@@ -0,0 +1,3 @@
+from _02_Intro2 import eleven_eleven
+print("Testing python installation")
+eleven_eleven("hello")
---
+++ _02_Intro2.py
@@ -0,0 +1,3 @@
+def eleven_eleven(n: str):
+    print("Testing python installation, module import, and function call")
+    print(f"Your var: {n}")
